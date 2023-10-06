17729ef12a8d1d8a93fe5064b1495205d65845d9
Nikos Kapatos <nikos@kapatos.com>
2023-10-06 19:50:53 +0200

    add macOS shell script to check and install xcode tools, rosetta (apple silicon), homebrew (installs ansible) and then proceed with the playbooks

---
+++ macos.sh
@@ -0,0 +1,41 @@
+echo "";
+
+#  Apple's command-line tools
+if type xcode-select >&- && xpath=$( xcode-select --print-path ) &&
+   test -d "${xpath}" && test -x "${xpath}" ; then
+   echo 'Apple Command-line tools already installed'
+else
+   xcode-select --install
+   # Enable command line tools
+   sudo xcode-select --switch /Library/Developer/CommandLineTools
+fi
+
+
+# Install rosetta
+/usr/bin/pgrep -q oahd && echo 'Rosetta already installed' || /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+
+# install homebrew
+which -s brew
+if [[ $? != 0 ]] ; then
+    # https://github.com/Homebrew/install/#install-homebrew-on-macos-or-linux
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+#    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+#    eval "$(/opt/homebrew/bin/brew shellenv)"
+
+    brew update && brew upgrade
+
+    # install required dependecies
+    brew install ansible
+fi
+
+# install ansible
+which -s ansible
+if [[ $? != 0 ]] ; then
+    brew install ansible
+fi
+
+# use installed python (not default from OS)
+#export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+#
+ansible-playbook init.yml
